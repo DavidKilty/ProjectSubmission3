f190c215b9aeac8653b377788ff73eaccbfd88e7
David Kilty <davidbkilty@gmail.com>
2024-06-07 15:47:33 +0000

    issues with IED so pushing to see if it resolves

--- run.py
+++ run.py
@@ -1,3 +1,58 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+# Constants
+BOARD_SIZE = 5
+SNAKE_LENGTHS = [2, 3]  # Example of snake lengths
+
+def create_board(size):
+    return [['~' for _ in range(size)] for _ in range(size)]
+
+def print_board(board):
+    for row in board:
+        print(' '.join(row))
+
+def snake(board, snake_lenght):
+    placed = False
+    while not placed:
+        orientation = random.choice(['H', 'V'])
+        if orientation == 'H':
+            row = random.randint(0, BOARD_SIZE - 1)
+            col = random.randint(0, BOARD_SIZE - snake_length)
+            if all(board[row][col + i] == '~' for i in range(snake_length)):
+                for i in range(snake_length):
+                    board[row][col + i] = 'S'
+                placed = True
+        else:
+            row = random.randint(0, BOARD_SIZE - snake_length)
+            col = random.randint(0, BOARD_SIZE - 1)
+            if all(board[row + i][col] == '~' for i in range(snake_length)):
+                for i in range(snake_length):
+                    board[row + i][col] = 'S'
+                placed = True
+
+def setup_board():
+    board = create_board(BOARD_SIZE)
+    for snake_length in SNAKE_LENGTHS:
+    place_snake(board, snake_length)
+    return board
+
+# Example usage
+player_board = setup_board()
+computer_board = setup_board()
+
+print("Player's Board:")
+print_board(player_board)
+print("\nComputer's Board:")
+print_board(computer_board)
+
+def take_shot(board, row, col):
+    if board[row][col] == 'S':
+        board[row][col] = 'X'  # Hit
+        return True
+    elif board[row][col] == '~':
+        board[row][col] = 'O'  # Miss
+        return False
+    return None  # Invalid shot
+def all_snakes_crushed(board):
+    return all(cell != 'S' for row in board for cell in row)
+
